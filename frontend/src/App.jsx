db1b529ce2ea36461eea093de87cd807db15a5bc
Dean Sallinen <deza604@gmail.com>
2018-11-11 17:42:06 -0800

    remove unused code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-// import axios from 'axios';
 import Helmet from 'react-helmet';
 import { request } from 'graphql-request';
 import Header from './components/header';
@@ -8,8 +7,6 @@ import Notes from './components/notesList';
 import NoteInput from './components/noteInput';
 import Toast from './components/toast';
 import Footer from './components/footer';
-
-// const API = process.env.REACT_APP_APIURL;
 
 class App extends Component {
   constructor(props) {
@@ -24,26 +21,10 @@ class App extends Component {
 
     this.onUserSelect = this.onUserSelect.bind(this);
     this.onNewNote = this.onNewNote.bind(this);
-    // this.getNotes = this.getNotes.bind(this);
     this.deleteNote = this.deleteNote.bind(this);
-    this.removeNote = this.removeNote.bind(this);
   }
 
   componentDidMount() {
-    // axios
-    //   .get(`http://${API}/v1/users/`)
-    //   .then((res) => {
-    //     const firstUser = res.data[0];
-    //     // console.log('first user', firstUser);
-    //     this.setState({ user: firstUser, users: res.data });
-    //     return firstUser;
-    //   })
-    //   .then(user => this.getNotes(user._id))
-    //   .catch((error) => {
-    //     console.error(error);
-    //     this.setState({ error });
-    //   });
-
     const query = `
       query Query {
         users {
@@ -80,7 +61,6 @@ class App extends Component {
   onNewNote(note) {
     const { user } = this.state;
     const { id } = user;
-    // console.log(id, note);
     this.setState({ isLoading: true });
 
     const query = `
@@ -101,7 +81,6 @@ class App extends Component {
     request('/.netlify/functions/graphql', query, { userID: id, note })
       .then((res) => {
         const newnote = res.createNote;
-        // console.log('mutation res', newnote);
         this.setState(state => ({
           isLoading: false,
           user: {
@@ -114,67 +93,9 @@ class App extends Component {
         console.error(error);
         this.setState({ error });
       });
-
-    // axios
-    //   .post(`http://${API}/v1/users/${userID}/notes/`, {
-    //     userID,
-    //     note,
-    //   })
-    //   .then((res) => {
-    //     console.log(res);
-    //     this.setState(prevState => ({
-    //       notes: [res.data, ...prevState.notes],
-    //       error: null,
-    //       isLoading: false,
-    //     }));
-    //   })
-    //   .catch((error) => {
-    //     console.error(error);
-    //     this.setState({ error, isLoading: false });
-    //   });
-  }
-
-  // getNotes(userID) {
-  //   this.setState({ isLoading: true });
-  //   axios
-  //     .get(`http://${API}/v1/users/${userID}/notes`)
-  //     .then(notes => this.setState({ notes: notes.data, isLoading: false }))
-  //     .catch((error) => {
-  //       console.error(error);
-  //       this.setState({ error, isLoading: false });
-  //     });
-
-  //   //   const query = `
-  //   //   query Query {
-  //   //     notes {
-  //   //       note
-  //   //       userID
-  //   //       createdAt
-  //   //       updatedAt
-  //   //     }
-  //   //   }
-  //   // `;
-
-  //   // request('/.netlify/functions/graphql', query)
-  //   //   .then((res) => {
-  //   //     console.log(res);
-  //   //     const { hello } = res;
-  //   //     this.setState({ hello, isLoading: false });
-  //   //   })
-  //   //   .catch((error) => {
-  //   //     console.error(error);
-  //   //     this.setState({ error });
-  //   //   });
-  // }
-
-  removeNote(id) {
-    // const notes = []
-    this.setState(prevState => ({ notes: prevState.notes.filter(note => note.id !== id) }));
   }
 
   deleteNote(noteID) {
-    // this.setState({ isLoading: true });
-
     const query = `
     mutation deleteNote($noteID: ID) {
       deleteNote(id: $noteID) {
@@ -189,32 +110,13 @@ class App extends Component {
     });
 
     this.setState(prevState => ({
-      // isLoading: false,
       user: {
         notes: prevState.user.notes.filter(note => note.id !== noteID),
       },
     }));
-    // const { id, userID, removeNote } = this.props;
-    // this.setState({ isLoading: true });
-    // axios
-    //   .delete(`http://${API}/v1/users/${userID}/notes/${id}`)
-    //   .then((res) => {
-    //     console.log('Note deleted', res);
-    //     removeNote(id);
-    //     this.setState(state => ({
-    //       //   // notes: [res.data, ...state.notes],
-    //       error: null,
-    //       isLoading: false,
-    //     }));
-    //   })
-    //   .catch((error) => {
-    //     console.error(error);
-    //     this.setState({ error, isLoading: false });
-    //   });
   }
 
   render() {
-    // console.log(this.state.user)
     const { users, user, error } = this.state;
     const { notes } = user;
     return (
