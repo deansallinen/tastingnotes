2a387b33a24283923ad54d251f93f39409d922e0
Dean Sallinen <deza604@gmail.com>
2019-02-14 13:16:56 -0800

    paginate note entry

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,21 +2,23 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Header = () => (
-  <header className={`py-4 bg-purple-dark`}>
-    <div className="container mx-auto flex justify-between items-center">
+  <header className="py-4 bg-purple-dark">
+    <div className="max-w-xl mx-auto flex justify-between items-center">
       <div>
-      <Link className='font-bold no-underline text-purple-lightest px-4' to="/">
-        TastingNotes
-      </Link>
-      <Link className='mr-4 no-underline hover:underline text-purple-lightest' to="/">Home</Link>
-      <Link className='mr-4 no-underline hover:underline text-purple-lightest' to="/events">Events</Link>
+        <Link className='font-bold no-underline text-purple-lightest pr-4' to="/">
+          TastingNotes
+        </Link>
+        <div className='inline-block ml-4'>
+          <Link className='mr-4 no-underline hover:underline text-purple-lightest' to="/">Home</Link>
+          <Link className='mr-4 no-underline hover:underline text-purple-lightest' to="/events">Events</Link>
+        </div>
       </div>
       <div>
         <Link to='/signup'>
-          <button className='mr-4 bg-purple-lightest hover:bg-white rounded shadow px-4 py-2 text-purple-dark font-bold'>Sign Up</button>
+          <button className='mr-4 bg-white hover:bg-white rounded shadow px-4 py-2 text-purple-dark font-bold'>Sign Up</button>
         </Link>
         <Link to='/login'>
-          <button className='mr-4 px-4 py-2 rounded text-purple-lightest hover:text-white font-bold'>Login</button>
+          <button className='px-4 py-2 rounded text-purple-lightest hover:text-white font-bold'>Login</button>
         </Link>
       </div>
     </div>
@@ -24,20 +26,32 @@ const Header = () => (
 )
 
 const Footer = () => (
-  <footer>
-    <Link to="/vendor">
-      <div className="inline-block mr-2">Vendor</div>
-    </Link>
-    <Link to="/organizer">
-      <div className="inline-block mr-2">Organizer</div>
-    </Link>
+  <footer className=' py-6 bg-purple-darkest text-purple '>
+    <div className='max-w-lg mx-auto'>
+      <Link to="/vendor">
+        <span className="mr-2">Vendor</span>
+      </Link>
+      <Link to="/organizer">
+        <span className=" mr-2">Organizer</span>
+      </Link>
+      <p>
+        TastingNotes is brought to you in part by:
+      </p>
+      <ul>
+        <li>React</li>
+        <li>Tailwind</li>
+        <li>GraphQL</li>
+        <li>Hasura</li>
+        <li>Postgres</li>
+      </ul>
+    </div>
   </footer>
 )
 
 const Layout = ({ children }) => (
   <div className="flex flex-col min-h-screen font-sans">
     <Header />
-    <div className="pt-6 bg-grey-lightest flex-1">
+    <div className="pt-6 pb-10 bg-grey-lightest flex-1">
       <div className="container mx-auto">{children}</div>
     </div>
     <Footer />
